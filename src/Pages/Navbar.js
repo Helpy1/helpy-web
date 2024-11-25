8aff91dbbbe3ec655ad6ca2634bc487cd8b52877
MS4136 <shoaib@helpyapp.co>
2024-11-25 20:09:54 +0500

    updated

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, Fragment,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import navlogo from '../Assets/logo-white.png';
 import { FaSistrix, FaHeart, FaMessage } from 'react-icons/fa6';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { FaHome } from "react-icons/fa";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { auth, signOut } from "../firebase/firebase-config";
 import { useNavigate } from 'react-router-dom';
-
+import { FaEye } from "react-icons/fa";
 const Navbar = ({onSearchClick }) => {
   const navigate = useNavigate();
-
   const [profileImage, setProfileImage] = useState('');
   const [name, setName] = useState('');
 
@@ -51,38 +51,49 @@ const Navbar = ({onSearchClick }) => {
   };
 
 
+
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+     <div className='md:shadow-sm'>
+     <nav className="bg-white border-gray-200 dark:bg-gray-900 md:block hidden">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/Home" className="flex items-center ">
             <img src={navlogo} className="h-auto w-32" alt="Flowbite Logo" />
             {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Helpy</span> */}
           </Link>
           <div className="items-center justify-between  w-full md:flex md:w-auto " id="navbar-language">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-10  md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <div
-                onClick={onSearchClick}
-                  className="flex flex-col gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-[#e9677b] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  onClick={onSearchClick}
+                  className="flex flex-row gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  <FaSistrix />
+                  <FaSistrix className='w-5'/>
                   <span>Search</span>
                 </div>
               </li>
               <li>
                 <Link
                   to="/Interest"
-                  className="flex flex-col gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-[#e9677b] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="flex flex-row gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   <FaHeart />
+                  <span>4U Ai</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Interest"
+                  className="flex flex-row gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  <FaEye  className='w-5'/>
                   <span>Interests</span>
                 </Link>
               </li>
               <li>
                 <Link
-                  to="/services"
-                  className="flex flex-col gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-[#e9677b] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  to="/Chat"
+                  className="flex flex-row gap-2 items-center justify-center py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent text-center md:hover:text-[#e9677b] dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   <FaMessage />
                   <span>Messages</span>
@@ -113,23 +124,13 @@ const Navbar = ({onSearchClick }) => {
                       <MenuItem>
                         {({ active }) => (
                           <a
-                            href="/Profiles"
+                            href="#"
                             className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'}`}
                           >
                             View Profile
                           </a>
                         )}
                       </MenuItem>
-                      {/* <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="/EditProfile"
-                            className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'}`}
-                          >
-                            Edit Profile
-                          </a>
-                        )}
-                      </MenuItem> */}
                       <MenuItem>
                         {({ active }) => (
                           <a
@@ -150,7 +151,27 @@ const Navbar = ({onSearchClick }) => {
                             href="#"
                             className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'}`}
                           >
-                            Get Account Verified
+                            Get ID Verified
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'}`}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'}`}
+                          >
+                            Help Center
                           </a>
                         )}
                       </MenuItem>
@@ -181,6 +202,27 @@ const Navbar = ({onSearchClick }) => {
 
         </div>
       </nav>
+      <nav className="fixed bottom-0 left-0 right-0 bg-white shadow-md border-t border-gray-200 md:hidden block">
+      <div className="flex justify-around items-center h-16">
+        {/* Home Icon */}
+        <Link className="bg-white shadow rounded-full p-4 hover:bg-[#e9677b] hover:text-white">
+        <FaHome />
+        </Link>
+        {/* Search Icon */}
+        <Link className="bg-white shadow rounded-full p-4 hover:bg-[#e9677b] hover:text-white">
+        <FaSistrix />
+        </Link>
+        {/* Notifications Icon */}
+        <Link className="bg-white shadow rounded-full p-4 hover:bg-[#e9677b] hover:text-white">
+        <FaHeart />
+        </Link>
+        {/* Profile Icon */}
+        <Link className="bg-white shadow rounded-full p-4 hover:bg-[#e9677b] hover:text-white">
+        <FaMessage />
+        </Link>
+      </div>
+    </nav>
+     </div>
     </>
   );
 };
